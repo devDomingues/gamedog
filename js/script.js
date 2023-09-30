709a342fafd2de2cca1f20485f59b94a857932e4
devDomingues <contact.guilhermed@gmail.com>
2023-09-29 21:53:38 -0300

    Script feito; gamedog V1 Finalizado.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 const dog = document.querySelector('.dog')
+const arbusto = document.querySelector('.arbusto')
+const nuvens = document.querySelector('.nuvens')
+const arvore = document.querySelector('.arvore')
+const explosao = document.querySelector('.explosao')
 
 
 document.addEventListener('keydown', jumpmove)
 document.addEventListener('touchstart', jumpmove)
 
+const loop = setInterval(() => {
+    const arbustopositon = arbusto.offsetLeft
+    const dogpositon = dog.offsetTop
+    const nuvensp = nuvens.offsetLeft
+    const arvorep = arvore.offsetLeft
+
+    if(dogpositon > 340 && arbustopositon <= 120 && arbustopositon >= 25 ) {
+        arbusto.style.left = `${arbustopositon}px`
+        nuvens.style.left = `${nuvensp}px`
+        arvore.style.left = `${arvorep}px`
+        dog.style.display = 'none'
+        explosao.style.display = 'block'
+        explosao.style.top = `${dogpositon}px`
+    }
+
+}, 10)
+
 
 function jumpmove(e) {
-    if(e.code === 'Space') {
+    if(e.code === 'Space' || e.code == 'touchstart') {
         dog.classList.add('jump')
         setTimeout(() => {
             dog.classList.remove('jump')
